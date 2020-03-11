a14f75fa23b1b637f6e0d8d1f926f2e826afc24c
Inzamam-ul haque <48618774+inzamam-inz@users.noreply.github.com>
2020-03-11 11:00:58 +0600

    Main input

---
+++ Output Show/LEXING/program.c
@@ -0,0 +1,61 @@
+
+
+double inzamam(int a, int b, double c)
+{
+    c = a+b;
+    return c;
+}
+
+int main()
+{
+    int a=5, b=0;
+
+    do
+    {
+        if(n> 0){
+            n=4;
+        }
+        a = a+1;
+        b = b+1;
+    }
+    while( a < 19 && b <= 3)
+
+    while( a < 19 || b <= 3)
+    {
+        if(n == a+b){
+            break;
+        }
+        a = a+1;
+        b = b+1;
+    }
+
+    if ( n> 0)
+    {
+        n=4;
+        n = n +1;
+    }
+    if (n==0){
+        n = n +1;
+    }
+    else if( n> 0)
+    {
+        n=5*3;
+    }
+    else if (n==0)
+        n=5;
+    else{
+        v=5*3;
+    }
+    else if(n >= 0)
+    {
+        n=4;
+        a = b + 2;
+    }
+
+    for(int a=0, b=0; a < 19 && b <= 3 || b==0 ; a++, b=a)
+    {
+        a = b + 2;
+    }
+
+    return 0;
+}
